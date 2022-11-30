783025dcef0ef0e012efe1a30f9c3e4a378a96bd
Barnabas Varga <vabarnabas@gmail.com>
2022-11-30 21:44:49 +0100

    Fixes

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  i18n: {
+    locales: ["hu-HU", "en-US"],
+    defaultLocale: "hu-HU",
+    localeDetection: false,
+  },
 }
 
 module.exports = nextConfig
